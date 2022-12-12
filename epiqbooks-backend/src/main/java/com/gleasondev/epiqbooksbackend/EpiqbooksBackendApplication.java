0253bcd787e78e34ef023bc001b4b1dbc07f36d8
Robert Gleason <l2ob122@gmail.com>
2022-12-11 19:26:14 -0800

    setup

---
+++ epiqbooks-backend/src/main/java/com/gleasondev/epiqbooksbackend/EpiqbooksBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gleasondev.epiqbooksbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpiqbooksBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpiqbooksBackendApplication.class, args);
+    }
+
+}
---
+++ epiqbooks-backend/src/test/java/com/gleasondev/epiqbooksbackend/EpiqbooksBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gleasondev.epiqbooksbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpiqbooksBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
