0c838dc703db58b75b279fc02012f8bdb0ab7fd8
Robert Gleason <l2ob122@gmail.com>
2023-01-24 19:45:26 -0800

    made a change to css on image on carosuel

--- epiqbooks-frontend/src/models/AddBookRequest.ts
+++ epiqbooks-frontend/src/models/AddBookRequest.ts
@@ -1,24 +1,37 @@
-class AddBookRequest {
-  title: string;
-  author: string;
-  description: string;
-  copies: number;
-  category: string;
-  img?: string;
+// class AddBookRequest {
+//   title: string;
+//   author: string;
+//   description: string;
+//   copies: number;
+//   category: string;
+//   img?: string;
 
+//   constructor(
+//     title: string,
+//     author: string,
+//     description: string,
+//     copies: number,
+//     category: string
+//   ) {
+//     this.title = title;
+//     this.author = author;
+//     this.description = description;
+//     this.copies = copies;
+//     this.category = category;
+//   }
+// }
+
+// export default AddBookRequest;
+
+class AddBookRequest {
   constructor(
-    title: string,
-    author: string,
-    description: string,
-    copies: number,
-    category: string
-  ) {
-    this.title = title;
-    this.author = author;
-    this.description = description;
-    this.copies = copies;
-    this.category = category;
-  }
+    public title: string,
+    public author: string,
+    public description: string,
+    public copies: number,
+    public category: string,
+    public img?: string,
+  ) {}
 }
 
 export default AddBookRequest;
