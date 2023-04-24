986a8b8582ced083e76fff5a9baa80762d8f35cd
Robert Gleason <l2ob122@gmail.com>
2023-04-23 17:10:31 -0700

    fixed heros and nav not loading propertly

--- epiqbooks-frontend/src/layouts/HomePage/components/LibraryServices.tsx
+++ epiqbooks-frontend/src/layouts/HomePage/components/LibraryServices.tsx
@@ -1,46 +1,47 @@
 import React from 'react';
-import { useOktaAuth } from '@okta/okta-react/';
-import { Link } from 'react-router-dom';
+
+import {Link} from 'react-router-dom';
+import {useAuth} from "../../../Auth/AuthContext";
 
 export const LibraryServices = () => {
-  // for our okta
-  const { authState } = useOktaAuth();
 
-  return (
-    <div className='container my-5'>
-      <div className='row p-4 align-items-center border shadow-lg'>
-        <div className='col-lg-7 p-3'>
-          <h1 className='display-4 fw-bold text-red-200'>
-            Cant' find what you're looking for?
-          </h1>
-          <p className='lead text-white'>
-            We're here to help. Our Admins are ready to assist you with your
-            research needs. Simply send them a personal message and they'll get
-            back to you as soon as possible.
-          </p>
-          <div className='d-grid gap-2 justify-content-md-start mb-4 mb-lg-3'>
-            {authState?.isAuthenticated ? (
-              <Link
-                to='/messages'
-                type='button'
-                className='bg-black text-white rounded-lg hover:bg-gray-500 pt-2
-            pb-2 pl-4 pr-4 mt-3'
-              >
-                Library Services
-              </Link>
-            ) : (
-              <Link
-                className='btn bg-black btn-lg hover:bg-gray-500 text-white mt-4'
-                to='/login/'
-              >
-                Sign up
-              </Link>
-            )}
-          </div>
+    const {isAuthenticated} = useAuth();
+
+    return (
+        <div className="container my-5">
+            <div className="row p-4 align-items-center border shadow-lg">
+                <div className="col-lg-7 p-3">
+                    <h1 className="display-4 fw-bold text-red-200">
+                        Cant' find what you're looking for?
+                    </h1>
+                    <p className="lead text-white">
+                        We're here to help. Our Admins are ready to assist you with your
+                        research needs. Simply send them a personal message and they'll get
+                        back to you as soon as possible.
+                    </p>
+                    <div className="d-grid gap-2 justify-content-md-start mb-4 mb-lg-3">
+                        {isAuthenticated ? (
+                            <Link
+                                to="/messages"
+                                type="button"
+                                className="bg-black text-white rounded-lg hover:bg-gray-500 pt-2
+            pb-2 pl-4 pr-4 mt-3"
+                            >
+                                Library Services
+                            </Link>
+                        ) : (
+                            <Link
+                                className="btn bg-black btn-lg hover:bg-gray-500 text-white mt-4"
+                                to="/login/"
+                            >
+                                Sign up
+                            </Link>
+                        )}
+                    </div>
+                </div>
+
+                <div className="col-lg-4 offset-lg-1 shadow-lg lost-image"></div>
+            </div>
         </div>
-
-        <div className='col-lg-4 offset-lg-1 shadow-lg lost-image'></div>
-      </div>
-    </div>
-  );
+    );
 };
--- epiqbooks-frontend/src/layouts/NavBarAndFooter/Navbar.tsx
+++ epiqbooks-frontend/src/layouts/NavBarAndFooter/Navbar.tsx
@@ -1,88 +1,87 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import { useOktaAuth } from '@okta/okta-react';
-import { LoadingSpinner } from '../Utils/LoadingSpinner';
+import {Link, NavLink} from 'react-router-dom';
+import {LoadingSpinner} from '../Utils/LoadingSpinner';
+import {useAuth} from "../../Auth/AuthContext";
 
 export function Navbar() {
-  // for our okta auth
-  const { oktaAuth, authState } = useOktaAuth();
 
-  // this is because it takes sometime to start up
-  if (!authState) {
-    return <LoadingSpinner />;
-  }
+    const {isAuthenticated, logout, user, login} = useAuth()
 
-  const handleLogout = async () => oktaAuth.signOut();
-  console.log(authState);
+    // this is because it takes sometime to start up
+    if (!isAuthenticated) {
+        return <LoadingSpinner/>;
+    }
+    const handleLogout = async () => logout();
+    console.log(user);
 
-  return (
-    <nav className='navbar navbar-expand-lg navbar-dark bg-dark py-5'>
-      {/* cuts off the corners of the page a little bit */}
-      <div className='container-fluid'>
-        <span className='navbar-brand'>Epiq Books</span>
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarNavDropDown'
-          aria-controls='navbarNavDropDown'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <span className='navbar-toggler-icon' />
-        </button>
-        <div className='navbar-collapse' id='navbarNavDropDown'>
-          <ul className='navbar-nav'>
-            <li className='nav-item'>
-              <Link className='nav-link' to='/'>
-                Home
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link className='nav-link' to='/search'>
-                Search Books
-              </Link>
-            </li>
-            {authState.isAuthenticated && (
-              <li>
-                <NavLink className='nav-link' to='/shelf'>
-                  Shelf
-                </NavLink>
-              </li>
-            )}
-            {authState.isAuthenticated &&
-              authState.accessToken?.claims.userType === 'admin' && (
-                <li className='nav-item'>
-                  <NavLink className='nav-link' to='/admin'>
-                    Admin
-                  </NavLink>
-                </li>
-              )}
-          </ul>
-          <ul className='navbar-nav ms-auto navbar-dark bg-dark'>
-            {!authState.isAuthenticated ? (
-              <li className='nav-item m-1'>
-                <Link
-                  type='button'
-                  className='btn btn-outline-light'
-                  to='/login'
-                >
-                  Sign In
-                </Link>
-              </li>
-            ) : (
-              <li>
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-5">
+            {/* cuts off the corners of the page a little bit */}
+            <div className="container-fluid">
+                <span className="navbar-brand">Epiq Books</span>
                 <button
-                  className='btn btn-outline-light'
-                  onClick={handleLogout}
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropDown"
+                    aria-controls="navbarNavDropDown"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
                 >
-                  Logout
+                    <span className="navbar-toggler-icon"/>
                 </button>
-              </li>
-            )}
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+                <div className="navbar-collapse" id="navbarNavDropDown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/search">
+                                Search Books
+                            </Link>
+                        </li>
+                        {isAuthenticated && (
+                            <li>
+                                <NavLink className="nav-link" to="/shelf">
+                                    Shelf
+                                </NavLink>
+                            </li>
+                        )}
+                        {isAuthenticated && user?.userType === "admin" && (
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/admin">
+                                    Admin
+                                </NavLink>
+                            </li>
+                        )}
+                    </ul>
+                    <ul className="navbar-nav ms-auto navbar-dark bg-dark">
+                        {isAuthenticated ? (
+                            <li className="nav-item m-1">
+                                <Link
+                                    type="button"
+                                    className="btn btn-outline-light"
+                                    to="/login"
+                                >
+                                    Sign In
+                                </Link>
+                            </li>
+                        ) : (
+                            <li>
+                                <button
+                                    className="btn btn-outline-light"
+                                    onClick={handleLogout}
+                                >
+                                    Logout
+                                </button>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 }
